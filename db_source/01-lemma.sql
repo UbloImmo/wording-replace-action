efed21d77f121d52df821b795acdc220d6fac652
blksnk <art.jnveigel@gmail.com>
2026-07-16 12:52:48 +0200

    chore(db): rename input seed files

---
+++ db_source/01-lemma.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0628e3224ac3aaee8739351c2adb11e2e14bf3bd5f0b556779e2945a88ccba09
+size 18922584
--- db_source/lemma.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:93dac9813c0aee0d0bda960ee6d9e94f0f2e8c68ddae638e6aea41fa770b4fa9
-size 18922582
