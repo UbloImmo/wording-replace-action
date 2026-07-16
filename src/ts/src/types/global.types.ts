51c062ce3ddee8f17041a04b95f1fd9130ebc8ce
blksnk <art.jnveigel@gmail.com>
2026-07-16 13:30:59 +0200

    chore: jsdoc

--- src/ts/src/types/global.types.ts
+++ src/ts/src/types/global.types.ts
@@ -1,7 +1,19 @@
+/**
+ * Array that must at lease contain one item
+ */
 export type NonEmptyArr<T> = [T, ...T[]];
 
+/**
+ * Either a non-empty array or an item
+ */
 export type ItemOrNonEmptyArr<T> = T | NonEmptyArr<T>;
 
+/**
+ * Either an item or an array that may be empty
+ */
 export type ItemOrArr<T> = T | T[];
 
+/**
+ * String that must at least include one character
+ */
 export type NonEmptyStr = Exclude<string, { length: 0 } | "">;
